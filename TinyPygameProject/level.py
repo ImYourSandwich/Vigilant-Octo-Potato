1d69c27a6ea126ca6c51854becbe754dcc41e088
Pierre PIGNÉ <POEI-Pierre@pockost.com>
2022-12-07 16:29:22 +0100

    Still doesn't work GG DAVID

--- TinyPygameProject/level.py
+++ TinyPygameProject/level.py
@@ -1,21 +1,22 @@
 import pygame
-from tiles iport Tile
+from tiles import Tile
+
 
 class Level:
-    def __init__(self,level_data,surface):
+    def __init__(self, level_data, surface):
         self.display_surface = surface
         self.level_data = level_data
-    
-    def setup_level(self,layout):
+
+    def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
-        for row_index,row in enumerate(layout):
-            for col_index,cell in enumerate(row):
+        for row_index, row in enumerate(layout):
+            for col_index, cell in enumerate(row):
                 if cell == 'X':
                     x = col_index * tile_size
                     y = row_index * tile_size
-                    tile = Tile((x,y),tile_size)
+                    tile = Tile((x, y), tile_size)
                     self.tiles.add(tile)
-    
+
     def run(self):
         self.tiles.update()
         self.tiles.draw(self.display_surface)
--- TinyPygameProject/main.py
+++ TinyPygameProject/main.py
@@ -1,23 +1,24 @@
-import pygame, sys
+import pygame
+import sys
 from settings import *
 from level import Level
 
-#Pygame setup
+# Pygame setup
 pygame.init()
 screen_width = 1200
 screen_height = 700
-screen = pygame.display.set_mode((screen_width,screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
-level = Level(level_map,screen)
+level = Level(level_map, screen)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    
+
     screen.fill('black')
     level.run()
-    
+
     pygame.display.update()
     clock.tick(60)
